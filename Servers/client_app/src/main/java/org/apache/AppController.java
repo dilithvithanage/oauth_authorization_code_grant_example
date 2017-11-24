6a0d695597ea5aa821a5783b45d9fb53727517b3
dilithvithanage <dilitherange@gmail.com>
2017-11-25 00:08:38 +0530

    First Upload

---
+++ Servers/client_app/src/main/java/org/apache/AppController.java
@@ -0,0 +1,17 @@
+package org.apache;
+
+/**
+ * Created by Dexter on 11/24/2017.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
